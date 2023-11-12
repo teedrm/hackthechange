d9e373fc5955d7fa61b148b50c7fba541bdf8fe9
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-11-12 02:53:54 -0700

    commit

--- src/Homepage.js
+++ src/Homepage.js
@@ -25,7 +25,7 @@ function Homepage() {
         <div className='transparent-circle'></div>
       </div>
       <div style={{ width:'110vw', marginLeft:"-42px", marginTop:"50px", display:'flex', flexDirection:' column', alignItems:'center', borderTop:'solid white 1px'}}>
-        <button style={{ background: 'none', borderTop: '2px solid black', borderBottom: '2px solid black', borderLeft: 'none', borderRight: 'none', marginTop:'20px', fontSize:' 1.3rem' , color:'white'}}>Partnerships</button>
+        <button style={{ background: 'none', borderTop: '2px solid black', borderBottom: '2px solid black', borderLeft: 'none', borderRight: 'none', marginTop:'20px', fontSize:' 1.3rem' , color:'black'}}>Partnerships</button>
         <div className='collabs' style={{ marginTop: "50px", marginBottom: '50px' }}>
           <img src='/OTP.png' alt='leaf' style={{ width: '80px',  }} />
           <img src='/TFL.png' alt='leaf' style={{ width: '80px', marginLeft: '30px' }} />
